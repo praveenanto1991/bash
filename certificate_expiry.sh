68aa209f4591a9d4113f863a1cf7197efab372c5
praveenanto1991 <86506813+praveenanto1991@users.noreply.github.com>
2021-07-23 11:56:01 +0530

    Create certificate_expiry.sh

---
+++ certificate_expiry.sh
@@ -0,0 +1,16 @@
+set -eu
+RED='\033[0;31m'
+domains=/path-to/domains.txt
+
+while read line; do
+   echo "checking if $line expires in less than 7 days"
+   expires=$(date -d "$(curl -ILkv --stderr - "$line" | grep  "expire date" | awk '{print $5 $6 $7}')" '+%s')
+   target=$(date -d "+7 days" '+%s')
+   #target=1721714966
+   #echo "target is $target"
+   #echo $expires
+   if [ "$target" -gt "$expires" ]
+   then
+     echo "$RED certificate expires in lees than 7 days on $(date -d @$expires '+%Y-%m-%d') $NOCOLOR";
+   fi
+done<$domains
