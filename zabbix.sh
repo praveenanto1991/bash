25eb0a89990dbeecd07ae473036aa6c4180bcece
praveenanto1991 <86506813+praveenanto1991@users.noreply.github.com>
2021-08-06 10:56:43 +0530

    Update zabbix.sh

--- zabbix.sh
+++ zabbix.sh
@@ -5,7 +5,16 @@ GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 #host=$1
 domains=/var/vcap/jobs/zabbix-server/helpers/domains.txt
-arrVar=()
+expired=()
+valid=()
+function output(){
+  if [[ ${expired[@]} ]]
+  then
+    echo "${expired[*]}"
+  else
+    echo "$valid"
+  fi
+}
 while read line; do
    #echo "checking if certificate of $line expires in less than 7 days"
    expires=$(date -d "$(echo | openssl s_client -connect "$line":443 -servername "$line" 2>/dev/null | openssl x509 -text | grep -i 'Not After' | awk '{print $4,$5,$7}')" '+%s')
@@ -15,14 +24,10 @@ while read line; do
    #echo $expires
    if [ "$target" -gt "$expires" ]
    then
-     arrVar+=("\"Certficate expires on ${line}\"")
-     #echo "\"Certficate expires on ${line}\""
-     #echo $arrVar
-     if [[ ${arrVar[@]} ]]
-     then
-       echo "$arrVar"
-     else
-       echo $arrVar
-     fi
-  fi
+     expired+=("\"Certficate expires on ${line}\"")
+   else
+     valid+=("\"Certficate is ok on ${line}\"")
+   fi
 done<$domains
+output
+
